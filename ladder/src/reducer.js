d84b33369dfb0f84ed150725b22771e9602c77e3
dastardlychimp <darien.hess@demochimp.com>
2016-10-15 11:06:33 -0600

    Creating webpack config and package.json

--- ladder/src/reducer.js
+++ ladder/src/reducer.js
@@ -1,4 +1,4 @@
-const challengeSpread = 2;
+const challengeSpread = 2
 
 const initialState = {
     playerOrder: [
@@ -15,39 +15,39 @@ const initialState = {
 function reducer(state = initialState, action) {
     switch(action.type) {
         case 'DECLARE_CHALLENGE':
-            return challenge(state, action);
+            return challenge(state, action)
         case 'RESULTS':
-            return results(state, action);
+            return results(state, action)
         default:
-            return state:
+            return state
     }
 }
 
 function results(state, action) {
-    let order = state.playerOrder;
-    const winner = action.winner;
-    const loser = action.loser;
-    order = moveLoserDown();
-    order = moveWinnerUp();
+    let order = state.playerOrder
+    const winner = action.winner
+    const loser = action.loser
+    order = moveLoserDown()
+    order = moveWinnerUp()
     return Object.assign({}, state, {
         playerOrder: order,
         pendingChallenge: false
     });
 
     function moveLoserDown(order, loser) {
-        const loserIndex = order.indexOf(loser);
+        const loserIndex = order.indexOf(loser)
         if (loserIndex + 1 !== order.length) {
             order.move(loserIndex, loserIndex + 1)
         }
-        return order;
+        return order
     }
     function moveWinnerUp(order, winner, loser) {
-        const winnerIndex = order.indexOf(winner);
-        const loserIndex = order.indexOf(loser);
+        const winnerIndex = order.indexOf(winner)
+        const loserIndex = order.indexOf(loser)
         if (winnerIndex - 1 >= 0) {
-            order.move(winnerIndex, loserIndex - 1);
+            order.move(winnerIndex, loserIndex - 1)
         }
-        return order;
+        return order
     }
 }
 
@@ -56,41 +56,21 @@ function challenge(state, action) {
         pendingChallenge: true,
         challenger: action.challenger,
         challenged: action.challenged
-    });
+    })
 }
 
 Array.prototype.move = function (oldIndex, newIndex) {
     if (newIndex >= this.length) {
         let neededSpots = newIndex - this.length;
         for (let i = 0; i < neededSpots; ++i) {
-            this.push(undefined);
+            this.push(undefined)
         }
     }
-    this.splice(new_index, 0, this.splice(oldIndex, 1)[0]);
-    return this;
+    this.splice(new_index, 0, this.splice(oldIndex, 1)[0])
+    return this
 }
 /*
 current reorder rules are:
     1. loser moves down a level switching with person below.
     2. Winner moves above loser.
-
-A
-B
-C
-D
-E
-
-D beats C
-A
-B
-D
-C
-E
-
-D beats B
-A
-C
-D
-B
-E
 */
